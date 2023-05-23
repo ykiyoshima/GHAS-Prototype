cae8147a7efb3d146fb638f81b35c3794f11a85a
ykiyoshima <y.kiyoshima.271@gmail.com>
2023-05-23 17:15:32 +0900

    initial commit

---
+++ GHAS-Prototype/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace GHAS_Prototype.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public string Title { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            return Page();
+        }
+    }
+}
